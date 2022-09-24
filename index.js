71ea66e990ad5cf8110c8f87655955bc278f72a5
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-09-23 22:56:11 -0500

    commit tree 8a167ee579ea267f258d246b05ac0f969a7d51be

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordArray = Array.from(word)
+  const arrayReverse = wordArray.reverse();
+  const joinerReverse = arrayReverse.join("")
+
+  if(word === joinerReverse) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  function isPalindrome(word) {
+    if (word === word.reverse() {
+    return true
+    }
+  }
 */
 
 /*
   Add written explanation of your solution here
+  Found a way with google to turn the word into an array, which gives me access to a method called reverse() which I then used to reverse the order. After that the commas were still in the way so I found another method called join() which I used to elminate the commas. After that I used an if else statement to determine whether the word passed in was a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
